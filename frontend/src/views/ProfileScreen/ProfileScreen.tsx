b95699bd3a27115f4c48cc0159610f9b207c0e52
Krzysztof <47855045+PiotrowskiKrzysztof@users.noreply.github.com>
2022-05-03 17:36:11 +0200

    Add ProfileScreen

--- frontend/src/views/ProfileScreen/ProfileScreen.tsx
+++ frontend/src/views/ProfileScreen/ProfileScreen.tsx
@@ -1,20 +1,80 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text } from '@ui-kitten/components';
+import { Text, Button } from '@ui-kitten/components';
+import { Control, FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import React from 'react';
-import { Page, Panel } from '../../components';
+import { StyleSheet } from 'react-native';
+import { 
+  Page,
+  Panel,
+  Input,
+  PasswordInput, 
+} from '../../components';
 import { RootStackParamList } from '../../utils/types';
+
+type Inputs = {
+  example: string;
+  password: string;
+};
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
 const ProfileScreen = ({ route, navigation }: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
   return (
     <Page navigation={navigation} route={route}>
       <Panel title='Profile'>
-        {/* TODO: Temporary data, remove it later */}
-        <Text>This is {route.params.name}'s profile | Some longer text lorem ipsum</Text>
+        <Input
+          name="example"
+          label="New Email"
+          control={control}
+          placeholder="email@edu.pl"
+          rules={{ required: 'This input is requred' }}
+          error={errors.example}
+          style={styles.gap}
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          control={control as unknown as Control<FieldValues, any>}
+          placeholder="*****"
+          error={errors.password}
+          style={styles.gap}
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          control={control as unknown as Control<FieldValues, any>}
+          placeholder="*****"
+          error={errors.password}
+          style={styles.gap}
+        />
+        <Button>
+          Save
+        </Button>
       </Panel>
     </Page>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 2,
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  gap: {
+    marginBottom: 30,
+  },
+});
 
 export default ProfileScreen;
